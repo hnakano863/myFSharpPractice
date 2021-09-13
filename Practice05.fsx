789fdc7fc764a93857523f3776ec4f9088ab2b3b
hnakano863 <notchi863@gmail.com>
2021-09-14 01:40:20 +0900

    うっかり問題の意味を間違えていたので修正

--- Practice05.fsx
+++ Practice05.fsx
@@ -1,19 +1,18 @@
 let fuzzyStartsWith (pat: string) (s: string): bool =
-    s.ToLower().StartsWith(pat.ToLower())
+    s.Trim().ToLower().StartsWith(pat.ToLower())
 
 let fuzzySearch pat = List.filter (fuzzyStartsWith pat)
 
 let input = [
     "TLV"
-    "tlv様"
-    "ミヤワキ様"
-    "テイエルブイ様"
-    "tlv御中"
-    "株式会社TLV"
+    "  tlv様"
+    "ミヤワキ様  "
+    "テイエルブイ様  "
+    "  tlv御中  "
+    " 株式会社TLV "
 ]
 
 let name = "Tlv"
 
-printfn "TLV"
 for x in fuzzySearch name input do
-    printfn "  %s" x
+    printfn "%s" x
